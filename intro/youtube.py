394b23510a6e87a9aadce16e687afcf1372a97d0
scsuarez <s.c.suarez@mac.com>
2015-02-13 15:19:03 -0500

    second commit, add text to youtube.py

---
+++ intro/youtube.py
@@ -0,0 +1,9 @@
+import json
+from urllib import urlopen
+url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json"
+response = urlopen(url)
+contents = response.read()
+text = contents.decode9('utf8')
+data = json.loads(text)
+for video in data['feed']['entry'][0:3]
+	print video['title']['$t']
