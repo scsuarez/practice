7e11dbd2c6bb7b8c6ec4bb8e097e4f5d3e6b2555
scsuarez <s.c.suarez@mac.com>
2015-02-13 15:22:13 -0500

    third commit, fixed syntax errors in code

--- intro/youtube.py
+++ intro/youtube.py
@@ -3,7 +3,7 @@ from urllib import urlopen
 url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json"
 response = urlopen(url)
 contents = response.read()
-text = contents.decode9('utf8')
+text = contents.decode('utf8')
 data = json.loads(text)
-for video in data['feed']['entry'][0:3]
+for video in data['feed']['entry'][0:3]:
 	print video['title']['$t']
