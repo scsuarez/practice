d1043fad5bf3de4c7a50f913b093a410f023deb1
scsuarez <s.c.suarez@mac.com>
2015-03-26 16:12:41 -0400

    added markup describing everything I would like to do with this file

--- Transcriptome Data/transcriptome search comparison.R
+++ Transcriptome Data/transcriptome search comparison.R
@@ -5,3 +5,5 @@ data1stLeg <- read.csv(1st_leg_transcriptome.csv, header = TRUE)
 #tell the program which column to search
 data1stLeg $Hit_desc.
 #how do I search for a phrase within a given column, such as "Ixodes scapularis"
+#count the number of results for the column and print it.
+#do this for all the files in "~/practice/Transcriptome Data"
