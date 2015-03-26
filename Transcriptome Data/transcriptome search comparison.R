566aa0788b59427a1475eb955d474f1fee2eff94
scsuarez <s.c.suarez@mac.com>
2015-03-26 15:58:50 -0400

    figuring out what to do

--- Transcriptome Data/transcriptome search comparison.R
+++ Transcriptome Data/transcriptome search comparison.R
@@ -1,3 +1,7 @@
+#tell the program which working directory
 setwd("~/practice/Transcriptome Data")
+#import the datafile we want to search
 data1stLeg <- read.csv(1st_leg_transcriptome.csv, header = TRUE)
-
+#tell the program which column to search
+data1stLeg $Hit_desc.
+#how do I search for a phrase within a given column, such as "Ixodes scapularis"
