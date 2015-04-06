8232b7498681e2b5decc6cfe4cf315169d131089
scsuarez <s.c.suarez@mac.com>
2015-04-06 18:49:39 -0400

    Completed HallerSearch.R and Added complete list of search terms

--- Transcriptome Data/HallerSearch.R
+++ Transcriptome Data/HallerSearch.R
@@ -1,5 +1,32 @@
+require(plyr)
 setwd("~/practice/Transcriptome Data")
-dat = read.csv(Hallers_organ_no_dup.csv, header = TRUE)
-# experiments begin here
+searchfile = "SearchTerm.csv"
+datfile = "Hallers_organ_no_dup.csv"
+dat = read.csv(datfile, header = TRUE)
+searchlist = read.csv(searchfile, header = TRUE)
+coltypes = lapply(dat,class)
+seqcolnames = c("Sequence.desc.","Hit.desc.")
 
-grepl(
+
+hits.search = ldply(seqcolnames, function(.colname) {
+    cat(sprintf('Processing %s\n', .colname))
+    .datcol = dat[[.colname]]
+    .outer.result = ddply(searchlist, c('Class', 'Keyword'), function(.searchdf) {
+        .term = .searchdf$Keyword
+        result = data.frame(rowid=grep(.term, .datcol))
+        return(result)
+    }, .progress='text')
+    .outer.result$searchCol = .colname
+    return(.outer.result)
+})
+
+hits.search = ddply(hits.search, c('Class', 'Keyword'), function(.hitsdf){
+    .rowid = unique(.hitsdf$rowid)
+    return(data.frame(rowid = .rowid))
+})
+hits.search = cbind(hits.search, dat[ hits.search$rowid, ])
+
+write.table(hits.search, file = "searchhits.csv", sep = ',', row.names = FALSE)
+
+
+    
